e0abd6eee116c618ce45e12eee20aff397a22c3f
Ariel Marcelo Pardo <adoorno@gmail.com>
2026-07-05 14:18:16 -0300

    Add handler and the first POST.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+
+	"clientesFrecuentes/internal/config"
+	"clientesFrecuentes/internal/handler"
+	"clientesFrecuentes/internal/repository"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 func main() {
+	connPool, err := pgxpool.NewWithConfig(context.Background(), config.Config())
+	if err != nil {
+		log.Fatal("Error while creating connection to the database!! ", err)
+	}
+
+	defer connPool.Close()
+
+	err = repository.CreateTableQuery(connPool)
+	if err != nil {
+		log.Fatal("Could not create the table: ", err)
+	}
+
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	h := handler.CustomerHandler{Pool: connPool}
+	router.POST("/customers", h.RegisterCustomer)
 	router.Run()
 }
---
+++ internal/config/db.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func Config() *pgxpool.Config {
+	const defaultMaxConns = int32(4)
+	const defaultMinConns = int32(0)
+	const defaultMaxConnLifetime = time.Hour
+	const defaultMaxConnIdleTime = time.Minute * 30
+	const defaultHealthCheckPeriod = time.Minute
+	const defaultConnectTimeout = time.Second * 5
+
+	const DATABASE_URL string = "postgres://loyalty:loyalty@localhost:5432/loyalty_dev"
+	dbConfig, err := pgxpool.ParseConfig(DATABASE_URL)
+	if err != nil {
+		log.Fatal("Failed to create a config, error: ", err)
+	}
+
+	dbConfig.MaxConns = defaultMaxConns
+	dbConfig.MinConns = defaultMinConns
+	dbConfig.MaxConnLifetime = defaultMaxConnLifetime
+	dbConfig.MaxConnIdleTime = defaultMaxConnIdleTime
+	dbConfig.HealthCheckPeriod = defaultHealthCheckPeriod
+	dbConfig.ConnConfig.ConnectTimeout = defaultConnectTimeout
+
+	return dbConfig
+}
